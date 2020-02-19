99dc70f9b6d603cd63348fcb2cadd98255df8691
Dany-obscure-tech <53596578+Dany-obscure-tech@users.noreply.github.com>
2020-02-19 15:40:54 +0500

    dany change 4 login implemented

---
+++ app/src/main/java/com/dotcom/rbs_system/SignInActivity.java
@@ -0,0 +1,83 @@
+package com.dotcom.rbs_system;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity {
+    EditText editText_email,editText_password;
+    Button button_signin;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        initialize();
+        signin();
+    }
+
+    private void initialize() {
+        editText_email = (EditText)findViewById(R.id.editText_email);
+        editText_password = (EditText)findViewById(R.id.editText_password);
+        button_signin = (Button)findViewById(R.id.button_signin);
+        button_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+
+    private void signin() {
+
+
+
+        String email, password;
+        email = editText_email.getText().toString();
+        password = editText_password.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+}
