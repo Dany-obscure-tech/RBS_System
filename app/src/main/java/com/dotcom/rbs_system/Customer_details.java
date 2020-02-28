3babc475ce2262e8c2ffce3f8a7e8c658d7e72d4
shahz <shahzaib.mehfooz175@gmail.com>
2020-02-28 14:53:09 +0500

    RBS-System (Shahzaib)

--- app/src/main/java/com/dotcom/rbs_system/Customer_details.java
+++ app/src/main/java/com/dotcom/rbs_system/Customer_details.java
@@ -1,14 +1,55 @@
 package com.dotcom.rbs_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Customer_details extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class Customer_details extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    ImageButton Back_btn;
+    Button date_btn;
+    TextView date_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_details);
+        date_btn=(Button)findViewById(R.id.date_btn);
+        Back_btn=(ImageButton)findViewById(R.id.Back_btn);
+        date_text=(TextView)findViewById(R.id.date_text);
+        Back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        date_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datepicker=new DatePickerFragment();
+                datepicker.show(getSupportFragmentManager(),"date picker");
+
+            }
+        });
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString= DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        date_text.setText(currentDateString);
+    }
+
 }
