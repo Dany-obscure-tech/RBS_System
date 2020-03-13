c26e9c5ef8c567ffc77f2ed4e5851ed377ab6890
Dany-obscure-tech <53596578+Dany-obscure-tech@users.noreply.github.com>
2020-03-13 12:51:49 +0500

    dany change 26 exchange activity incomplete

--- app/src/main/java/com/dotcom/rbs_system/Exchange.java
+++ app/src/main/java/com/dotcom/rbs_system/Exchange.java
@@ -1,5 +1,6 @@
 package com.dotcom.rbs_system;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,23 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exchange extends AppCompatActivity {
     ImageButton Back_btn,sms_btn,gmail_btn;
+    DatabaseReference buyList_ref;
+
+    List<String> categoryList;
+    List<String> purchasePriceList;
+    List<String> exisitngItemsKeyIDList;
+
 
 
     @Override
@@ -18,10 +34,57 @@ public class Exchange extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
 
+        initialize();
+
+
+
+        getItemsKeyIDList();
+        onClickListeners();
+
+
+
+    }
+
+    private void initialize() {
+        buyList_ref = FirebaseDatabase.getInstance().getReference("Buy_list");
+
         Back_btn=(ImageButton)findViewById(R.id.Back_btn);
         gmail_btn=(ImageButton) findViewById(R.id.gmail_btn);
         sms_btn=(ImageButton)findViewById(R.id.sms_btn);
 
+        exisitngItemsKeyIDList = new ArrayList<>();
+        categoryList = new ArrayList<>();
+        purchasePriceList = new ArrayList<>();
+    }
+
+    private void getItemsKeyIDList() {
+        buyList_ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String id;
+                for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()) {
+                    id = String.valueOf(exisitngItemsKeyIDList.add(String.valueOf(dataSnapshot1.child("Item_keyID").getValue())));
+                    for (int i = 0;i<exisitngItemsKeyIDList.size()+1;i++){
+
+                    }
+                    exisitngItemsKeyIDList.add(String.valueOf(dataSnapshot1.child("Item_keyID").getValue()));
+                    purchasePriceList.add(String.valueOf(dataSnapshot1.child("Purchase_price").getValue()));
+                }
+                gettingItemCategoryAndDetails();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void gettingItemCategoryAndDetails() {
+
+    }
+
+    private void onClickListeners() {
         Back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,8 +112,5 @@ public class Exchange extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
     }
 }
