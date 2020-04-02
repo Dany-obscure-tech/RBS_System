d407cb2da585a07863e1f9360b659dd9d5bcafee
Dany-obscure-tech <53596578+Dany-obscure-tech@users.noreply.github.com>
2020-04-03 04:04:55 +0500

    dany commit 28

--- app/src/main/java/com/dotcom/rbs_system/Accessories.java
+++ app/src/main/java/com/dotcom/rbs_system/Accessories.java
@@ -2,16 +2,24 @@ package com.dotcom.rbs_system;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dotcom.rbs_system.Adapter.AdapterAccessoriesPurchaseRecyclerView;
 import com.dotcom.rbs_system.Model.SampleSearchModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,30 +28,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat;
 import ir.mirrajabi.searchdialog.core.BaseSearchDialogCompat;
 import ir.mirrajabi.searchdialog.core.SearchResultListener;
 
-public class Accessories extends AppCompatActivity {
+public class Accessories extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
 
-    Button stock_entry_btn,sale_btn;
-    LinearLayout upperLayout,lowerLayout;
+    Button stock_entry_btn,sale_btn,purchase_btn;
+    LinearLayout upperLayout,lowerLayout,purchaseLayout;
 
-    DatabaseReference categoryRef,reference;
+    RecyclerView purchase_recyclerView;
+    AdapterAccessoriesPurchaseRecyclerView adapterAccessoriesPurchaseRecyclerView;
+
+    DatabaseReference categoryRef,reference,accessoriesCategoryRef;
     List<String> categoryList;
-    Button selectCategory_btn,submit_btn;
+    Button selectCategory_btn,submit_btn,purchaseSubmit_btn;
     Button selectCategory_btn2,searchForItem_btn,submit_btn2;
+    Button purchaseAddItem_btn;
     RatingBar ratingBar;
 
     ImageButton Back_btn;
 
     List<String> exisitngItemsList,exisitngItemsIDList,exisitngItemsKeyIDList,exisitngItemsCategoryList,existingItemsConditionsList;
+    List<String> accesoriesCategoriesList;
+
+    List<String> purchaseItemNameList,purchaseCategoryList,purchasePriceUnitList,purchaseQuantityList;
 
     EditText itemName_editText,itemId_editText,purchase_price_editText;
+
+    Dialog purchaseItemAddAlert;
+    Dialog purchaseCategoryAddAlert;
 
     DatabaseReference existingItemsRef;
     private String itemKeyID;
@@ -51,10 +71,30 @@ public class Accessories extends AppCompatActivity {
 
     String firebaseAuthUID = String.valueOf(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
+
+    EditText companyName_editText,paidAmount_editText,invoiceNo_editText;
+    TextView purchaseDate_textView;
+
+    Button purchaseAlertCategory_btn,purchaseAlertCategoryAdd_btn,purchaseAlertenter_btn,purchaseAlertCancel_btn,date_btn;
+
+    EditText priceUnit_editText,quantity_editText,alertItemName_editText;
+
+    EditText alertAddCategoryName_editText;
+    Button alertAddCategoryEnter_btn,alertAddCategoryCancel_btn;
+
     private ArrayList<SampleSearchModel> createCategoryData(){
         ArrayList<SampleSearchModel> items = new ArrayList<>();
         for (int i=0;i<categoryList.size();i++){
             items.add(new SampleSearchModel(categoryList.get(i),null,null,null,null,null,null,null));
+        }
+
+        return items;
+    }
+
+    private ArrayList<SampleSearchModel> createAccessoriesCategoryData(){
+        ArrayList<SampleSearchModel> items = new ArrayList<>();
+        for (int i=0;i<accesoriesCategoriesList.size();i++){
+            items.add(new SampleSearchModel(accesoriesCategoriesList.get(i),null,null,null,null,null,null,null));
         }
 
         return items;
@@ -85,17 +125,51 @@ public class Accessories extends AppCompatActivity {
 
     private void initialize() {
 
+
+        purchaseItemAddAlert = new Dialog(this);
+        purchaseItemAddAlert.setContentView(R.layout.alert_purchase_add_item);
+
+        purchaseCategoryAddAlert = new Dialog(this);
+        purchaseCategoryAddAlert.setContentView(R.layout.alert_purchase_add_category);
+
+        purchaseAlertCategoryAdd_btn = (Button)purchaseItemAddAlert.findViewById(R.id.purchaseAlertCategoryAdd_btn);
+        purchaseAlertCancel_btn = (Button)purchaseItemAddAlert.findViewById(R.id.purchaseAlertCancel_btn);
+        purchaseAlertenter_btn = (Button)purchaseItemAddAlert.findViewById(R.id.purchaseAlertenter_btn);
+
+        alertItemName_editText = (EditText) purchaseItemAddAlert.findViewById(R.id.alertItemName_editText);
+        purchaseAlertCategory_btn = (Button)purchaseItemAddAlert.findViewById(R.id.purchaseAlertCategory_btn);
+        quantity_editText = (EditText) purchaseItemAddAlert.findViewById(R.id.quantity_editText);
+        priceUnit_editText = (EditText) purchaseItemAddAlert.findViewById(R.id.priceUnit_editText);
+
+        alertAddCategoryName_editText = (EditText) purchaseCategoryAddAlert.findViewById(R.id.alertAddCategoryName_editText);
+        alertAddCategoryEnter_btn  = (Button) purchaseCategoryAddAlert.findViewById(R.id.alertAddCategoryEnter_btn);
+        alertAddCategoryCancel_btn = (Button) purchaseCategoryAddAlert.findViewById(R.id.alertAddCategoryCancel_btn);
+
+        purchaseItemNameList = new ArrayList<>();
+        purchaseCategoryList = new ArrayList<>();
+        purchasePriceUnitList = new ArrayList<>();
+        purchaseQuantityList = new ArrayList<>();
+
+        purchase_recyclerView = (RecyclerView)findViewById(R.id.purchase_recyclerView);
+        adapterAccessoriesPurchaseRecyclerView = new AdapterAccessoriesPurchaseRecyclerView(Accessories.this,purchaseItemNameList,purchaseCategoryList,purchasePriceUnitList,purchaseQuantityList);
+        purchase_recyclerView.setAdapter(adapterAccessoriesPurchaseRecyclerView);
+        purchase_recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+
+
         Back_btn=(ImageButton)findViewById(R.id.Back_btn);
 
         reference = FirebaseDatabase.getInstance().getReference();
         categoryRef = FirebaseDatabase.getInstance().getReference("Categories");
+        accessoriesCategoryRef = FirebaseDatabase.getInstance().getReference("Accessories_categories");
         categoryList = new ArrayList<>();
 
         stock_entry_btn = (Button)findViewById(R.id.stock_entry_btn);
         sale_btn = (Button)findViewById(R.id.sale_btn);
+        purchase_btn = (Button)findViewById(R.id.purchase_btn);
 
         upperLayout = (LinearLayout) findViewById(R.id.upperLayout);
         lowerLayout = (LinearLayout) findViewById(R.id.lowerLayout);
+        purchaseLayout = (LinearLayout) findViewById(R.id.purchaseLayout);
 
         selectCategory_btn = (Button)findViewById(R.id.selectCategory_btn);
         selectCategory_btn2 = (Button)findViewById(R.id.selectCategory_btn2);
@@ -113,6 +187,9 @@ public class Accessories extends AppCompatActivity {
 
         submit_btn = (Button)findViewById(R.id.submit_btn);
         submit_btn2 = (Button)findViewById(R.id.submit_btn2);
+        purchaseSubmit_btn = (Button)findViewById(R.id.purchaseSubmit_btn);
+
+        purchaseAddItem_btn = (Button)findViewById(R.id.purchaseAddItem_btn);
 
         existingItemsRef = FirebaseDatabase.getInstance().getReference("Accessories");
 
@@ -120,7 +197,18 @@ public class Accessories extends AppCompatActivity {
         exisitngItemsIDList = new ArrayList<>();
         exisitngItemsKeyIDList = new ArrayList<>();
         exisitngItemsCategoryList = new ArrayList<>();
-        existingItemsConditionsList= new ArrayList<>();
+        existingItemsConditionsList = new ArrayList<>();
+
+        accesoriesCategoriesList = new ArrayList<>();
+
+        companyName_editText  = (EditText)findViewById(R.id.companyName_editText);
+        paidAmount_editText = (EditText)findViewById(R.id.paidAmount_editText);
+        invoiceNo_editText = (EditText)findViewById(R.id.invoiceNo_editText);
+        purchaseDate_textView = (TextView) findViewById(R.id.purchaseDate_textView);
+
+        date_btn = (Button) findViewById(R.id.date_btn);
+
+
     }
 
     private void getCategoryList() {
@@ -129,6 +217,21 @@ public class Accessories extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
                     categoryList.add(String.valueOf(dataSnapshot1.getValue()));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        accessoriesCategoryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int i =0;
+                for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    accesoriesCategoriesList.add(String.valueOf(dataSnapshot1.getValue()));
                 }
             }
 
@@ -169,22 +272,70 @@ public class Accessories extends AppCompatActivity {
         finish();
     }
 
+    private void detailsSubmitPurchaseAlert() {
+
+        purchaseItemNameList.add(alertItemName_editText.getText().toString());
+        purchaseCategoryList.add(purchaseAlertCategory_btn.getText().toString());
+        purchasePriceUnitList.add(priceUnit_editText.getText().toString());
+        purchaseQuantityList.add(quantity_editText.getText().toString());
+
+        //todo
+        adapterAccessoriesPurchaseRecyclerView.notifyDataSetChanged();
+        purchaseItemAddAlert.dismiss();
+
+    }
+
+    private void detailsSubmitPurchase() {
+
+        String key = reference.push().getKey();
+        reference.child("Accessories_purchase_list").child(key).child("Company_name").setValue(companyName_editText.getText().toString());
+        reference.child("Accessories_purchase_list").child(key).child("Invoice_date").setValue(purchaseDate_textView.getText().toString());
+        reference.child("Accessories_purchase_list").child(key).child("Paid_amount").setValue(paidAmount_editText.getText().toString());
+        reference.child("Accessories_purchase_list").child(key).child("Invoice_no").setValue(invoiceNo_editText.getText().toString());
+
+        for (int i = 0;i<purchaseItemNameList.size();i++){
+            reference.child("Accessories_purchase_list").child(key).child("Items").child("Item_"+String.valueOf(i+1)).child("Item_name").setValue(purchaseItemNameList.get(i));
+            reference.child("Accessories_purchase_list").child(key).child("Items").child("Item_"+String.valueOf(i+1)).child("Item_category").setValue(purchaseCategoryList.get(i));
+            reference.child("Accessories_purchase_list").child(key).child("Items").child("Item_"+String.valueOf(i+1)).child("Item_price_unit").setValue(purchasePriceUnitList.get(i));
+            reference.child("Accessories_purchase_list").child(key).child("Items").child("Item_"+String.valueOf(i+1)).child("Item_quantity").setValue(purchaseQuantityList.get(i));
+        }
+
+        reference.child("Accessories_purchase_list").child(key).child("added_by").setValue(firebaseAuthUID);
+
+        finish();
+
+    }
+
     private void layoutToggle() {
         stock_entry_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                purchaseLayout.setVisibility(View.GONE);
                 upperLayout.setVisibility(View.VISIBLE);
                 lowerLayout.setVisibility(View.GONE);
+
             }
         });
 
         sale_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                purchaseLayout.setVisibility(View.GONE);
                 upperLayout.setVisibility(View.GONE);
                 lowerLayout.setVisibility(View.VISIBLE);
+
             }
         });
+
+        purchase_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                purchaseLayout.setVisibility(View.VISIBLE);
+                upperLayout.setVisibility(View.GONE);
+                lowerLayout.setVisibility(View.GONE);
+            }
+        });
+
     }
 
     private void fetchingExisitingItems(String category) {
@@ -240,12 +391,42 @@ public class Accessories extends AppCompatActivity {
     }
 
     private void onClickListeners() {
+
+        date_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datepicker=new DatePickerFragment();
+                datepicker.show(getSupportFragmentManager(),"date picker");
+
+            }
+        });
+
         Back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        purchaseAlertCategory_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SimpleSearchDialogCompat(Accessories.this, "Search...",
+                        "What are you looking for...?", null, createAccessoriesCategoryData(),
+                        new SearchResultListener<SampleSearchModel>() {
+                            @Override
+                            public void onSelected(BaseSearchDialogCompat dialog,
+                                                   SampleSearchModel item, int position) {
+                                purchaseAlertCategory_btn.setText(item.getTitle());
+                                purchaseAlertCategory_btn.setBackgroundColor(getResources().getColor(R.color.colorLightGrey));
+                                purchaseAlertCategory_btn.setTextColor(getResources().getColor(R.color.textGrey));
+
+                                dialog.dismiss();
+                            }
+                        }).show();
+            }
+        });
+
 
         selectCategory_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -289,6 +470,7 @@ public class Accessories extends AppCompatActivity {
             }
         });
 
+
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -304,6 +486,78 @@ public class Accessories extends AppCompatActivity {
                     detailsSubmit2();
             }
         });
+
+        purchaseAddItem_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                purchaseItemAddAlert.setCancelable(false);
+                purchaseItemAddAlert.show();
+            }
+        });
+
+
+        purchaseAlertCancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                purchaseItemAddAlert.dismiss();
+            }
+        });
+
+        purchaseAlertenter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validatePurchaseAlertFields() == true)
+                    detailsSubmitPurchaseAlert();
+            }
+        });
+
+        purchaseSubmit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validatePurchaseFields() == true)
+                    detailsSubmitPurchase();
+            }
+        });
+
+        purchaseAlertCategoryAdd_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                purchaseCategoryAddAlert.setCancelable(false);
+                purchaseCategoryAddAlert.show();
+            }
+        });
+
+        alertAddCategoryEnter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = alertAddCategoryName_editText.getText().toString();
+                Boolean valid = true;
+                for (int i = 0;i<accesoriesCategoriesList.size();i++){
+
+                    if (s.equals(accesoriesCategoriesList.get(i))){
+                        alertAddCategoryName_editText.setError("Category already exists");
+                        valid = false;
+                    }
+                }
+                if (valid==true){
+                    String key = accessoriesCategoryRef.push().getKey();
+                    accessoriesCategoryRef.child(key).setValue(s);
+                    finish();
+                }
+            }
+        });
+
+        alertAddCategoryCancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                purchaseCategoryAddAlert.dismiss();
+            }
+        });
+
+
     }
 
     private boolean validateFields() {
@@ -367,4 +621,64 @@ public class Accessories extends AppCompatActivity {
         return valid;
     }
 
+    private boolean validatePurchaseAlertFields() {
+        boolean valid = true;
+
+        if (purchaseAlertCategory_btn.getText().toString().equals("Select Category")) {
+            Toast.makeText(this, "Please select category", Toast.LENGTH_LONG).show();
+            valid = false;
+        }
+
+        if (alertItemName_editText.getText().toString().isEmpty()) {
+            alertItemName_editText.setError("Please enter item name");
+            valid = false;
+        }
+        if (priceUnit_editText.getText().toString().isEmpty()) {
+            priceUnit_editText.setError("Please enter price unit");
+            valid = false;
+        }
+        if (quantity_editText.getText().toString().isEmpty()) {
+            quantity_editText.setError("Please enter quantity");
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    private boolean validatePurchaseFields() {
+        Boolean valid = true;
+
+        if (purchaseDate_textView.getText().toString().equals("Select date")) {
+            Toast.makeText(this, "Select date", Toast.LENGTH_LONG).show();
+            valid = false;
+        }
+        if (companyName_editText.getText().toString().isEmpty()) {
+            companyName_editText.setError("Please enter company name");
+            valid = false;
+        }
+        if (paidAmount_editText.getText().toString().isEmpty()) {
+            paidAmount_editText.setError("Please enter paid amount");
+            valid = false;
+        }
+        if (invoiceNo_editText.getText().toString().isEmpty()) {
+            invoiceNo_editText.setError("Please enter invoice no");
+            valid = false;
+        }
+        if (purchaseItemNameList.size()==0){
+            Toast.makeText(this, "Please enter Items", Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString= DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        purchaseDate_textView.setText(currentDateString);
+    }
 }
