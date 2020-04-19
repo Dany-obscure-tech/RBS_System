46a33528e9c60d71212750df0340c2d1259704c0
shahz <shahzaib.mehfooz175@gmail.com>
2020-04-19 17:45:21 +0500

    shahzaib work done

--- app/src/main/java/com/dotcom/rbs_system/MainActivity.java
+++ app/src/main/java/com/dotcom/rbs_system/MainActivity.java
@@ -1,17 +1,30 @@
 package com.dotcom.rbs_system;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.dotcom.rbs_system.Classes.Exchanged_itemdata;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+//Navigation drawer
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    //Navigation drawer
 
     CardView BUY,Sale,repair,Exchange,Accessories,Settings;
     ImageButton icon1,icon2,icon3,icon5,icon4,icon6;
@@ -19,6 +32,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Nav drawer
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Intent intent;
+                switch(id)
+                {
+                    case R.id.nav_profile:
+                        Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_settings:
+                        intent = new Intent(MainActivity.this, Settings.class);
+                        startActivity(intent);
+                        closeDrawer();
+                        return true;
+                    case R.id.nav_logout:
+                        Toast.makeText(MainActivity.this, "My Cart",Toast.LENGTH_SHORT).show();break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+
+            private void closeDrawer() {
+                if (dl.isDrawerOpen(GravityCompat.START)) {
+                    dl.closeDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+
+
         BUY=(CardView)findViewById(R.id.BUY);
         Sale=(CardView)findViewById(R.id.Sale);
         repair=(CardView)findViewById(R.id.repair);
@@ -131,5 +184,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(t.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
