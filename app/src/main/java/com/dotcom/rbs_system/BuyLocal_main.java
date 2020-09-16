0891006384431c6b72239a2545e4f08ba251cd0d
shahz <shahzaib.mehfooz175@gmail.com>
2020-09-16 21:23:30 +0500

    Shahzaib

--- app/src/main/java/com/dotcom/rbs_system/BuyLocal_main.java
+++ app/src/main/java/com/dotcom/rbs_system/BuyLocal_main.java
@@ -1,14 +1,40 @@
 package com.dotcom.rbs_system;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BuyLocal_main extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    NavController fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_local_main);
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        //        To remove default colors of icons
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        // Switch to page one
+                        break;
+                    case R.id.profile:
+                        // Switch to page two
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
