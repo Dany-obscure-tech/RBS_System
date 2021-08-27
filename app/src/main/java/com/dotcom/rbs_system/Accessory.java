88059bacfae40fd4c8de87c80a4575af431645d9
Shahzaib_Precision <ShahzaibMehfooz>
2021-08-27 19:00:03 +0500

    Styling completed by Shahzaib

--- app/src/main/java/com/dotcom/rbs_system/Accessory.java
+++ app/src/main/java/com/dotcom/rbs_system/Accessory.java
@@ -1,14 +1,107 @@
 package com.dotcom.rbs_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.dotcom.rbs_system.Adapter.Adapter_RBS_accessories_inventory_RecyclerView;
+import com.dotcom.rbs_system.Adapter.Adapter_Vendor_inventory_RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Accessory extends AppCompatActivity {
+
+    RecyclerView shopkeeper_accessories_stock_recyclerview;
+    TextView order_accessory_textview, sale_accessory_textview, add_accessory_textview;
+    ImageButton back_btn;
+    Adapter_RBS_accessories_inventory_RecyclerView adapter_rbs_accessories_inventory_recyclerView;
+    List<String> accessory_name_textView, accessory_inventory_Category_textView, stockPrice_list, accessory_inventory_Quantity_textView, stockkeyId_list, edit_stock_textview_list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accessory);
+
+        initialization();
+        onclicklistners();
+
+
+    }
+
+    private void onclicklistners() {
+        order_accessory_textview_listner();
+        sale_accessory_textview_listner();
+        add_accessory_textview_listner();
+        back_btn_listner();
+    }
+
+    private void back_btn_listner() {
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void add_accessory_textview_listner() {
+        add_accessory_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Accessory.this, Accessory_add.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void sale_accessory_textview_listner() {
+        sale_accessory_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Accessory.this, Accessory_sale.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void order_accessory_textview_listner() {
+        order_accessory_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Accessory.this, RBS_Vendors.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initialization() {
+        accessory_name_textView = new ArrayList<>();
+        stockPrice_list = new ArrayList<>();
+        accessory_inventory_Quantity_textView = new ArrayList<>();
+        stockkeyId_list = new ArrayList<>();
+        edit_stock_textview_list = new ArrayList<>();
+        accessory_inventory_Category_textView = new ArrayList<>();
+        accessory_inventory_Category_textView.add("Laptop");
+        accessory_name_textView.add("Asus ROG");
+        stockPrice_list.add("1500");
+        accessory_inventory_Quantity_textView.add("10");
+        stockkeyId_list.add("ID");
+        //TODO id ko check kar ley
+        shopkeeper_accessories_stock_recyclerview = findViewById(R.id.shopkeeper_accessories_stock_recyclerview);
+        shopkeeper_accessories_stock_recyclerview.setLayoutManager(new GridLayoutManager(Accessory.this, 1));
+        order_accessory_textview = findViewById(R.id.order_accessory_textview);
+        sale_accessory_textview = findViewById(R.id.sale_accessory_textview);
+        add_accessory_textview = findViewById(R.id.add_accessory_textview);
+        back_btn = findViewById(R.id.back_btn);
+        adapter_rbs_accessories_inventory_recyclerView = new Adapter_RBS_accessories_inventory_RecyclerView(Accessory.this, accessory_name_textView, accessory_inventory_Category_textView, stockPrice_list, accessory_inventory_Quantity_textView, stockkeyId_list, null);
+        shopkeeper_accessories_stock_recyclerview.setAdapter(adapter_rbs_accessories_inventory_recyclerView);
     }
 }
---
+++ app/src/main/java/com/dotcom/rbs_system/Adapter/Adapter_RBS_accessories_inventory_RecyclerView.java
@@ -0,0 +1,209 @@
+package com.dotcom.rbs_system.Adapter;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dotcom.rbs_system.Classes.Currency;
+import com.dotcom.rbs_system.Classes.VendorStockDetails;
+import com.dotcom.rbs_system.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Adapter_RBS_accessories_inventory_RecyclerView extends RecyclerView.Adapter<Adapter_RBS_accessories_inventory_RecyclerView.MyViewHolder> {
+    Context context;
+    List<String> accessory_name_textView;
+    List<String> accessory_inventory_Category_textView;
+    List<String> stockPrice_list;
+    List<String> accessory_inventory_Quantity_textView;
+    List<String> edit_stock_textview_list;
+    List<String> stockkeyId_list;
+
+    Dialog dialog_box;
+
+    Boolean validate = true;
+
+    public Adapter_RBS_accessories_inventory_RecyclerView(Context context, List<String> accessory_name_textView, List<String> accessory_inventory_Category_textView, List<String> stockPrice_list, List<String> accessory_inventory_Quantity_textView, List<String> stockkeyId_list, List<String> edit_stock_textview_list) {
+        this.context = context;
+        this.accessory_name_textView = accessory_name_textView;
+        this.accessory_inventory_Category_textView = accessory_inventory_Category_textView;
+        this.stockPrice_list = stockPrice_list;
+        this.accessory_inventory_Quantity_textView = accessory_inventory_Quantity_textView;
+        this.stockkeyId_list = stockkeyId_list;
+        this.edit_stock_textview_list = edit_stock_textview_list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.shopkeeper_accesory_inventory_item, parent, false);
+        Adapter_RBS_accessories_inventory_RecyclerView.MyViewHolder myViewHolder = new Adapter_RBS_accessories_inventory_RecyclerView.MyViewHolder(v);
+
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
+        holder.accessory_name_textView.setText(accessory_name_textView.get(position));
+        holder.accesory_inventory_Category_textView.setText(accessory_inventory_Category_textView.get(position));
+        holder.stockPrice_textView.setText(stockPrice_list.get(position));
+        holder.accessory_inventory_Quantity_textView.setText(accessory_inventory_Quantity_textView.get(position));
+
+        holder.currency_textView.setText(Currency.getInstance().getCurrency());
+
+        holder.edit_stock_textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog_box = new Dialog(context);
+                dialog_box.setContentView(R.layout.alert_rbs_accessory_stock_edit);
+
+                holder.item_name_textview = dialog_box.findViewById(R.id.item_name_textview);
+                holder.category_editText = dialog_box.findViewById(R.id.username_editText);
+                holder.remove_btn = dialog_box.findViewById(R.id.remove_btn);
+                holder.save_btn = dialog_box.findViewById(R.id.save_btn);
+                holder.cancel_btn = dialog_box.findViewById(R.id.cancel_btn);
+                holder.item_price_editText = dialog_box.findViewById(R.id.item_price_editText);
+                holder.item_quantity_edittext = dialog_box.findViewById(R.id.item_quantity_edittext);
+
+                holder.category_editText.setText(accessory_inventory_Category_textView.get(position));
+
+                holder.item_price_editText.setText(stockPrice_list.get(position));
+                holder.item_quantity_edittext.setText(accessory_inventory_Quantity_textView.get(position));
+
+                itemPriceTextWatcher(holder.item_price_editText);
+                ItemQuantityTextWatcher(holder.item_quantity_edittext);
+
+
+                holder.save_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (validate) {
+                            //TODO Daniyal: yaha pa name bhi edit ho sakey ye bhi check kar, aur delete btn bhi connect karna ha, category bhi connect nhi ha
+                            // todo Shahzaib : validation for quantity and price
+//                            ((Activity) context).recreate();
+                        }
+
+                    }
+                });
+
+                holder.cancel_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog_box.dismiss();
+                    }
+                });
+
+
+                dialog_box.show();
+
+            }
+
+            private void ItemQuantityTextWatcher(final EditText item_quantity_edittext) {
+                item_quantity_edittext.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if (accessory_inventory_Quantity_textView.get(position).equals(item_quantity_edittext.getText().toString())) {
+                            item_quantity_edittext.setError("Please enter new value!");
+                            validate = false;
+                        }
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        if (item_quantity_edittext.getText().toString().isEmpty()) {
+                            item_quantity_edittext.setError("Please enter value!");
+                            validate = false;
+                        }
+                    }
+                });
+            }
+
+            private void itemPriceTextWatcher(final EditText item_price_editText) {
+                item_price_editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if (stockPrice_list.get(position).equals(item_price_editText.getText().toString())) {
+                            item_price_editText.setError("Please enter new value!");
+                            validate = false;
+                        }
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+
+                        if (item_price_editText.getText().toString().isEmpty()) {
+                            item_price_editText.setError("Please enter value!");
+                            validate = false;
+                        } else {
+                            if (Float.parseFloat(item_price_editText.getText().toString()) == 0.0f) {
+                                item_price_editText.setError("Please enter valid value!");
+                                validate = false;
+                            }
+                        }
+                    }
+                });
+            }
+
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        return accessory_inventory_Category_textView.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView accessory_name_textView, accesory_inventory_Category_textView, stockPrice_textView, accessory_inventory_Quantity_textView;
+        TextView currency_textView;
+        TextView edit_stock_textview;
+        TextView item_name_textview, category_editText;
+        EditText item_price_editText, item_quantity_edittext;
+        TextView save_btn, cancel_btn, remove_btn;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            accessory_name_textView = itemView.findViewById(R.id.accessory_name_textView);
+            accesory_inventory_Category_textView = itemView.findViewById(R.id.accesory_inventory_Category_textView);
+            stockPrice_textView = itemView.findViewById(R.id.stockPrice_textView);
+            accessory_inventory_Quantity_textView = itemView.findViewById(R.id.accessory_inventory_Quantity_textView);
+            currency_textView = itemView.findViewById(R.id.currency_textView);
+            edit_stock_textview = itemView.findViewById(R.id.edit_stock_textview);
+        }
+    }
+}
